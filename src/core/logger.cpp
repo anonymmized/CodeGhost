dcf3d2624ac6dbd5145749e82981467af492154b
Homelande-77 <a1337@Egors-MacBook-Pro.local>
2026-04-26 00:26:34 +0300

    L7 issue: logger logic

---
+++ src/core/logger.cpp
@@ -0,0 +1,45 @@
+#include "logger.hpp"
+#include <chrono>
+#include <ctime>
+#include <iomanip>
+#include <sstream>
+
+namespace Logger {
+  Logger* globalLogger = nullptr;
+  Logger::Logger(const std::string &filename, std::ostream &out) : filename(std::move(filename)), out(&out) {
+    if (!this->filename.empty()) file_out.open(this->filename, std::ios::app);
+  }
+  void Logger::logMessage(const LogLevel &level, const std::string &message, FileInfo extraInfo) {
+    std::string formedText = formLog(level, message, extraInfo);
+    if (out) {
+      *out << formedText;
+    }
+  };
+  
+  std::string Logger::formLog(const LogLevel &level, const std::string &message, FileInfo extraInfo){
+    std::stringstream ss;
+    auto now = std::chrono::system_clock::now();
+    std::time_t now_time = std::chrono::system_clock::to_time_t(now);
+    ss << "[" << std::put_time(std::localtime(&now_time), "%Y-%m-%d %H:%M:%S") << "] ";
+    switch (level) {
+    case INFO:  ss << "[INFO] ";  break;
+    case WARN:  ss << "[WARN] ";  break;
+    case ERROR: ss << "[ERROR] "; break;
+    }
+    if (!extraInfo.path.empty()) ss << "[" << extraInfo.path << ":" << " ]";
+    ss << message << "\n";
+    return ss.str();
+   };
+
+  void Logger::writeLogToFile(const LogLevel &level, const std::string &message, FileInfo extraInfo){
+    std::lock_guard<std::mutex> lock(mtx);
+    if (!file_out.is_open() && !filename.empty()) {
+      file_out.open(filename, std::ios::app);
+    }
+    std::string log_message = formLog(level, message, extraInfo);
+    if (file_out.is_open()) {
+      file_out << log_message;
+      file_out.flush();
+    }
+  };
+}
---
+++ src/core/logger.hpp
@@ -0,0 +1,55 @@
+#ifndef LOGGER_HPP
+#define LOGGER_HPP
+#include <string>
+#include <iostream>
+#include <unordered_map>
+#include <cerrno>
+#include <fstream>
+#include <mutex>
+
+
+namespace Logger {
+  struct FileInfo {
+    std::string path;
+    int line = 0;
+    
+  };
+  
+  class Logger {
+  private:
+    std::mutex mtx;
+    std::string filename;
+    std::ostream* out;
+    std::ofstream file_out;
+  public:
+    enum LogLevel {INFO, WARN, ERROR};
+    explicit Logger(const std::string& filename = "", std::ostream &out = std::cout);
+    Logger(const Logger&) = delete;
+    Logger& operator=(const Logger&) = delete;
+    std::string formLog(const LogLevel &level,
+			const std::string &message,
+			FileInfo extraInfo
+			);
+    void logMessage(const LogLevel &level,
+		    const std::string &message,
+		    FileInfo extraInfo
+		    );
+    void writeLogToFile(const LogLevel &level,
+			const std::string &message,
+			FileInfo extraInfo
+			);
+  };
+  #define LOG_MESSAGE(logger, level, message, extraInfo) logger.logMessage(level, message, extraInfo)
+  #define LOG_INFO(logger, message, extraInfo) logger.logMessage(Logger::INFO, message, extraInfo)
+  #define LOG_WARN(logger, message, extraInfo) logger.logMessage(Logger::WARN, message, extraInfo)
+  #define LOG_ERROR(logger, message, extraInfo) logger.logMessage(Logger::ERROR, message, extraInfo)
+
+  #define LOG_MESSAGE_G(level, message, extraInfo) if(Logger::globalLogger) Logger::globalLogger->logMessage(level, message, extraInfo)
+  #define LOG_INFO_G(message, extraInfo) if(Logger::globalLogger) Logger::globalLogger->logMessage(Logger::INFO, message, extraInfo)
+  #define LOG_WARN_G(message, extraInfo) if(Logger::globalLogger) Logger::globalLogger->logMessage(Logger::WARN, message, extraInfo)
+  #define LOG_ERROR_G(message, extraInfo) if(Logger::globalLogger) Logger::globalLogger->logMessage(Logger::ERROR, message, extraInfo)
+  
+  extern Logger* globalLogger;
+};
+
+#endif
