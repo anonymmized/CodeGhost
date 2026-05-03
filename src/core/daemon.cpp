5975bb9e25269be143b80b66abf90a1d0d45cc2b
anonymmized <lih0deev@inbox.ru>
2026-05-03 19:06:16 +0300

    basic json upload to file

--- src/core/daemon.cpp
+++ src/core/daemon.cpp
@@ -4,43 +4,94 @@
 #include <fcntl.h>
 #include <string>
 #include <fstream>
-#include <sys/inotify.h>
+//#include <sys/inotify.h>
+#include <vector>
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::ordered_json;
+
+struct Config {
+    std::vector<std::string> watch_paths;
+    std::vector<std::string> ignore_paths;
+    std::vector<std::string> critical_paths;
+    int start_hour;
+    int end_hour;
+    bool recursive = true;
+};
 
 void daemonise(bool silent = true) {
-    pid_t pid = fork(); // create subprocess
-    if (pid < 0) exit(EXIT_FAILURE); // the proccess wasn't created
+    pid_t pid = fork();
+    if (pid < 0) exit(EXIT_FAILURE);
     if (pid > 0) {
         std::cout << "daemon PID: " << pid << '\n';
-        exit(EXIT_SUCCESS); // ends current proccess with zero code
+        exit(EXIT_SUCCESS);
     }
 
-    setsid(); // creates new session and proccess group. disconect with terminal
-    if (silent) { // check on flag
-        int fd = open("/dev/null", O_RDWR); // /dev/null opend for read and write
-        // move all data from 0/1/2 to fd (/dev/null)
+    setsid();
+    if (silent) {
+        int fd = open("/dev/null", O_RDWR);
         dup2(fd, 0);
         dup2(fd, 1);
         dup2(fd, 2);
     }
 }
 
+/*
+Config loadFromConfig(const std::string& path) {
+
+    Config conf;
+
+}
+*/
+
+void uploadToConfig(const Config& conf) {
+    json j;
+    for (int i = 0; i < conf.watch_paths.size(); i++) {
+        j["watch_paths"].push_back(conf.watch_paths[i]);
+    }
+    for (int i = 0; i < conf.ignore_paths.size(); i++) {
+        j["ignore_paths"].push_back(conf.ignore_paths[i]);
+    }
+    for (int i = 0; i < conf.critical_paths.size(); i++) {
+        j["critical_paths"].push_back(conf.critical_paths[i]);
+    }
+    j["start_hour"] = conf.start_hour;
+    j["end_hour"] = conf.end_hour;
+    j["recursive"] = conf.recursive;
+    std::ofstream outfile("config.json");
+    if (!outfile.is_open()) {
+        throw std::runtime_error("The config.json wasn't opened");
+    }
+    outfile << j.dump(4);
+    outfile.close();
+}
+/*
+std::vector<std::string> getArgs(const char **argv) {
+
+}
+*/
+
 int main(int argc, char **argv) {
-    int fd = inotify_init(); // initialise inotify saving file descriptor (fd)
-    // add file descriptor to inotify watch function and sub on create and delete flags
+
+    Config conf = {{".", "../"}, {"/tmp", "/var"}, {"/root", "/user"}, 10, 20, true};
+    uploadToConfig(conf);
+    std::cout << "The config was upload to file\n";
+    /*
+    int fd = inotify_init();
     int wd = inotify_add_watch(fd, "./", IN_CREATE | IN_DELETE);
-    char buffer[4096]; // create buffer to read
+    char buffer[4096];
     if (argc == 2 && std::string(argv[1]) == "--daemonise") {
         std::cout << "daemon starting...\n";
         daemonise();
         while (true) {
-            int len = read(fd, buffer, sizeof(buffer)); // read length to buffer
+            int len = read(fd, buffer, sizeof(buffer));
             int i = 0;
             while (i < len) {
-                auto* event = reinterpret_cast<inotify_event*>(&buffer[i]); // inotify_event structure with main fields
+                auto* event = reinterpret_cast<inotify_event*>(&buffer[i]);
                 if (event->len) {
                     if (event->mask & IN_CREATE) {
                         std::ofstream infile("./logs.txt", std::ios::app);
-                        infile << "Created: " << event->name << '\n'; // write logs to file
+                        infile << "Created: " << event->name << '\n';
                         infile.close();
                     }
                     if (event->mask & IN_DELETE) {
@@ -49,9 +100,11 @@ int main(int argc, char **argv) {
                         infile.close();
                     }
                 }
-                i += sizeof(inotify_event) + event->len; // move index to end of the read segment
+                i += sizeof(inotify_event) + event->len;
             }
         }
     }
+    */
+
     return 0;
 }
