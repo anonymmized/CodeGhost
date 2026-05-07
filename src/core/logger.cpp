5119c02e25b35e9c8163fb9ce898206cdb3a87cb
anonymmized <lih0deev@inbox.ru>
2026-05-07 11:41:33 +0300

    logger class was implemented

--- src/core/logger.cpp
+++ src/core/logger.cpp
@@ -6,18 +6,33 @@
 #include <iostream>
 #include <fstream>
 
-void log(LogLevel level, const std::string& str, const LogSettings& settings, std::ostream& logfile) {
+int checkStream(std::ostream& file) {
+    if (!file.is_open()) {
+        openlog("daemon", LOG_PID | LOG_CONS, LOG_DAEMON);
+        syslog(LOG_ERR, "Failed to open logfile: %s", logpath.c_str());
+        std::cerr << "Failed to open logfile: " << logpath << '\n';
+        closelog();
+        return 1;
+    }
+    return 0;
+}
+
+void Logger::log(const std::string& str) {
+    std::ofstream file(path, std::ios::app | std::ios::out);
+    if (checkStream(file)) {
+        throw std::runtime_error("The logfile was not opened");
+    }
     auto now = std::chrono::system_clock::now();
     std::time_t t = std::chrono::system_clock::to_time_t(now);
     std::tm tm{};
     localtime_r(&t, &tm);
 
     uint32_t lvl = static_cast<uint32_t>(level);
-    if (level >= settings.log_level)
+    if (level >= log_level)
         logfile << std::put_time(&tm, "%d.%m.%y %H:%M:%S") << strLevels[lvl] << str << '\n';
 
-    if (level >= settings.tty_level) {
-        if (settings.colored)
+    if (level >= tty_level) {
+        if (colored)
             std::cout << LOG_COLORS[lvl] << std::put_time(&tm, "%d.%m.%y %H:%M:%S") << strLevels[lvl] << CLR << str << '\n';
         else
             std::cout << std::put_time(&tm, "%d.%m.%y %H:%M:%S") << strLevels[lvl] << str << '\n';
--- src/core/logger.hpp
+++ src/core/logger.hpp
@@ -4,23 +4,38 @@
 #include <cstdint>
 #include <array>
 
-class LogSettings {
-    public:
+class Logger {
+    private:
         uint8_t log_level : 2;
         uint8_t tty_level : 2;
         uint8_t colored   : 1;
         uint8_t timestamp : 1;
         uint8_t reserved  : 2;
-        constexpr LogSettings(uint8_t _log_level = LOG_INFO,
-                              uint8_t _tty_level = LOG_INFO,
-                              bool _colored = true,
-                              bool _timestamp = true) noexcept
+        LogLevel level;
+        std::string path;
+    public:
+        uint8_t getLogLevel() { return log_level; }
+        uint8_t getTtyLevel() { return tty_level; }
+        uint8_t getColored() { return colred; }
+        uint8_t getTimestamp() { return timestamp; }
+        uint8_t getReserved() { return reserved; }
+        constexpr Logger(uint8_t _log_level = LOG_INFO,
+                         uint8_t _tty_level = LOG_INFO,
+                         bool _colored = true,
+                         bool _timestamp = true,
+                         LogLevel _level,
+                         std::string _path) noexcept
             : log_level(_log_level & 0x03),
               tty_level(_tty_level & 0x03),
               colored(_colored ? 1u : 0u),
               timestamp(_timestamp ? 1u : 0u),
-              reserved(0)
+              reserved(0),
+              level(_level),
+              path(_path)
+
         {}
+        void log(const std::string& str);
+        int checkStream(std::ostream& file);
 };
 
 enum LogLevel {
@@ -44,4 +59,4 @@ constexpr std::array<std::string_view, 3> LogColors = {
 
 
 
-void log(LogLevel level, const std::string& str, const LogSettings& settings, std::ostream& logfile);
+//void log(LogLevel level, const std::string& str, const LogSettings& settings, std::ostream& logfile);
