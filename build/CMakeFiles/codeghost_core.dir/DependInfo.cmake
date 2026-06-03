c6b55c3d66bc889b8181b7691add455e64a1f3be
anonymmized <lih0deev@inbox.ru>
2026-06-03 10:11:12 +0300

    cmakelists was modified

---
+++ build/CMakeFiles/codeghost_core.dir/DependInfo.cmake
@@ -0,0 +1,29 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/cli/cli.cpp" "CMakeFiles/codeghost_core.dir/src/cli/cli.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/cli/cli.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/core/daemon.cpp" "CMakeFiles/codeghost_core.dir/src/core/daemon.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/core/daemon.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/core/eventprocessor.cpp" "CMakeFiles/codeghost_core.dir/src/core/eventprocessor.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/core/eventprocessor.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/core/hasher.cpp" "CMakeFiles/codeghost_core.dir/src/core/hasher.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/core/hasher.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/core/logger.cpp" "CMakeFiles/codeghost_core.dir/src/core/logger.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/core/logger.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/core/watcher.cpp" "CMakeFiles/codeghost_core.dir/src/core/watcher.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/core/watcher.cpp.o.d"
+  "/Users/insany/Documents/osnova/proga/cpp/CodeGhost/src/utils/utils.cpp" "CMakeFiles/codeghost_core.dir/src/utils/utils.cpp.o" "gcc" "CMakeFiles/codeghost_core.dir/src/utils/utils.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/codeghost_core.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for codeghost_core.
+# This may be replaced when dependencies are built.
